64cc906fdb95cb7f06cd5afdb35ade4d00460dc2
Chloé Danisevicius <chloe.danisevicius@gmail.com>
2021-04-23 18:47:12 +0200

    fix buttons

--- src/components/Socials/socials.js
+++ src/components/Socials/socials.js
@@ -4,14 +4,14 @@ import github from "../../assets/images/github.svg";
 import linkedin from "../../assets/images/linkedin.svg";
 import twitter from "../../assets/images/twitter.svg";
 
-const SocialStyles = styled.div`
+const SocialStyles = styled.section`
   width: 100%;
   display: flex;
   justify-content: center;
   padding: 0.7rem 0;
   border-top: 1px dotted #e4507550;
 
-  a {
+  .social-buttons {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -20,10 +20,9 @@ const SocialStyles = styled.div`
     border-radius: 4px;
     margin: 0.2rem;
     transition: background-color 0.3s;
-  }
-
-  a:hover {
+    &:hover {
     background-color: hsla(204, 45%, 98%, 0.15);
+    }
   }
 
   img {
@@ -34,13 +33,13 @@ const SocialStyles = styled.div`
 
 const Socials = () => (
   <SocialStyles>
-    <a href="https://github.com/SpookyUmi">
+    <a href="https://github.com/SpookyUmi" className="social-buttons">
       <img src={github} alt="github logo" />
     </a>
-    <a href="https://www.linkedin.com/in/spookyumi/">
+    <a href="https://www.linkedin.com/in/spookyumi/" className="social-buttons">
       <img src={linkedin} alt="linkedin logo" />
     </a>
-    <a href="https://twitter.com/Spooky_Umi">
+    <a href="https://twitter.com/Spooky_Umi" className="social-buttons">
       <img src={twitter} alt="twitter logo" />
     </a>
   </SocialStyles>
